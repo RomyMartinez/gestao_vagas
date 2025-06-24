2f64edabd8a26775d675725e70b089380f674b17
RomyMartinez <ngcsmrtnz@gmail.com>
2025-06-24 16:50:15 -0300

    Create CanditateEntity.java

---
+++ src/main/java/com/romy/gestao_vagas/modules/canditate/CanditateEntity.java
@@ -0,0 +1,32 @@
+package com.romy.gestao_vagas.modules.canditate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class CanditateEntity {
+
+    private UUID id;
+    private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message ="O campo (username) não deve conter espaços")
+    private String username;
+    
+    @NotBlank(message = "O campo (email) não deve estar vazios")
+    @Email(message = "O campo (email) deve conter um e-mail valido")
+    private String email;
+
+    @Length(min = 10, max =100, message ="Deve conter mais 10 caracteres")
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
