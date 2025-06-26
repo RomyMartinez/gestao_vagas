5a3ebe003a81044e0c1427b81c5925e300c4f607
RomyMartinez <ngcsmrtnz@gmail.com>
2025-06-26 11:35:04 -0300

    Create CandidateRepository.java

---
+++ src/main/java/com/romy/gestao_vagas/modules/canditate/CandidateRepository.java
@@ -0,0 +1,12 @@
+package com.romy.gestao_vagas.modules.canditate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String Email);
+    Optional<CandidateEntity> findByUsername(String username);
+
+}
