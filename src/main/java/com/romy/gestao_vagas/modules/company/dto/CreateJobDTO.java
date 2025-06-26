d14510b6bd82f4ab1c7ff0404fa68eaa429962fd
RomyMartinez <ngcsmrtnz@gmail.com>
2025-06-26 11:34:22 -0300

    Create CreateJobDTO.java

---
+++ src/main/java/com/romy/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,11 @@
+package com.romy.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    private String benefits;
+    private String description;
+    private String level;
+
+}
