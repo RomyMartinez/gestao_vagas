e169add592bdf3b740c1075645533c06a6ed01b4
RomyMartinez <ngcsmrtnz@gmail.com>
2025-06-26 11:32:20 -0300

    Create UseCase Company

---
+++ src/main/java/com/romy/gestao_vagas/modules/company/useCase/AuthCompanyUseCase.java
@@ -0,0 +1,62 @@
+package com.romy.gestao_vagas.modules.company.useCase;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.romy.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.romy.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
+import com.romy.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+    
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+            () -> {
+                throw new UsernameNotFoundException("Company não foi encontrado ou senha errada");
+            }
+        );
+
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+        if(!passwordMatches){
+            throw new AuthenticationException(); 
+        }
+
+        var duration = Instant.now().plus(Duration.ofHours(2));
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javavags")
+            .withExpiresAt(duration)
+            .withClaim("roles", Arrays.asList("COMPANY")) // Assuming a single role for companies
+            .withSubject(company.getId().toString())
+            .sign(algorithm);
+        
+
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+            .access_token(token)
+            .expires_in(duration.toEpochMilli()) // 2 hours in seconds
+            .build();
+
+        return authCompanyResponseDTO;
+    }   
+}
---
+++ src/main/java/com/romy/gestao_vagas/modules/company/useCase/CreateJobUseCase.java
@@ -0,0 +1,17 @@
+package com.romy.gestao_vagas.modules.company.useCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.romy.gestao_vagas.modules.company.entities.JobEntity;
+import com.romy.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity){
+        return this.jobRepository.save(jobEntity); 
+    }
+}
